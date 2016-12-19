37827c35520ea2d3a6757b7f90b807217c7a6df1
YUDHA <yudhapratama539@gmail.com>
2016-12-20 00:11:15 +0700

    up lagi

--- app/src/main/java/com/rackspira/dompetku/database/DbHelper.java
+++ app/src/main/java/com/rackspira/dompetku/database/DbHelper.java
@@ -15,15 +15,11 @@ public class DbHelper extends SQLiteOpenHelper{
     private static final String DATABASE_NAME="dompet";
     private static final int DATABASE_VERSION=1;
 
-    protected static final String TABLE_PEMASUKKAN="pemasukkan";
-    protected static final String TABLE_PENGELUARAN="pengeluaran";
+    protected static final String TABLE_INPUT="inputan";
 
-    private static final String ID_MASUK="id_masuk";
-    private static final String ID_KELUAR="id_keluar";
-    protected static final String KET_MASUK="ket_masuk";
-    protected static final String KET_KELUAR="ket_keluar";
-    protected static final String BIAYA_MASUK="biaya_masuk";
-    protected static final String BIAYA_KELUAR="biaya masuk";
+    private static final String ID="id";
+    protected static final String KET="ket";
+    protected static final String BIAYA="biaya";
 
     private static DbHelper dbHelper;
 
@@ -33,23 +29,16 @@ public class DbHelper extends SQLiteOpenHelper{
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("CREATE TABLE "+TABLE_PEMASUKKAN+" ("+
-                ID_MASUK+" INTEGER PRIMARY KEY AUTO INCREMENT, "+
-                KET_MASUK+" TEXT, "+
-                BIAYA_MASUK+" INTEGER);");
-
-        db.execSQL("CREATE TABLE "+TABLE_PENGELUARAN+" ("+
-                ID_KELUAR+" INTEGER PRIMARY KEY AUTO INCREMENT, "+
-                KET_KELUAR+" TEXT, "+
-                BIAYA_KELUAR+" INTEGER);");
+        db.execSQL("CREATE TABLE "+TABLE_INPUT+" ("+
+                ID+" INTEGER PRIMARY KEY AUTO INCREMENT, "+
+                KET+" TEXT, "+
+                BIAYA+" INTEGER);");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int lama, int baru) {
         if(lama != baru){
-            db.execSQL("DROP TABLE IF EXISTS"+TABLE_PEMASUKKAN);
-
-            db.execSQL("DROP TABLE IF EXISTS"+TABLE_PENGELUARAN);
+            db.execSQL("DROP TABLE IF EXISTS"+TABLE_INPUT);
 
             onCreate(db);
         }
