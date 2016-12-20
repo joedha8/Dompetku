7899f36ad1b40b83bd323ded2c8eb932ed8c7d92
Blank15 <yudistirosaputro@gmail.com>
2016-12-20 10:35:41 +0700

    Icon Add

--- app/src/main/java/com/rackspira/dompetku/MainActivity.java
+++ app/src/main/java/com/rackspira/dompetku/MainActivity.java
@@ -84,11 +84,11 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_home) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_pemasukkan) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_pengeluaran) {
 
         } else if (id == R.id.nav_manage) {
 
--- app/src/main/java/com/rackspira/dompetku/model/AmbilData.java
+++ app/src/main/java/com/rackspira/dompetku/model/AmbilData.java
@@ -6,8 +6,8 @@ package com.rackspira.dompetku.model;
 
 public class AmbilData {
 
-    private String Keterangan1,Keterangan2;
-    private int masuk,keluar;
+    private String Keterangan1;
+    private int masuk;
 
     public String getKeterangan1() {
         return Keterangan1;
@@ -17,27 +17,11 @@ public class AmbilData {
         Keterangan1 = keterangan1;
     }
 
-    public String getKeterangan2() {
-        return Keterangan2;
-    }
-
-    public void setKeterangan2(String keterangan2) {
-        Keterangan2 = keterangan2;
-    }
-
     public int getMasuk() {
         return masuk;
     }
 
     public void setMasuk(int masuk) {
         this.masuk = masuk;
-    }
-
-    public int getKeluar() {
-        return keluar;
-    }
-
-    public void setKeluar(int keluar) {
-        this.keluar = keluar;
     }
 }
